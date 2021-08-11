3a3e24078de733cb0d35b8e7e82baa6e200f50f1
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2021-08-11 13:12:08 -0800

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -1,3 +1,32 @@
+Time limit: 1.00 s Memory limit: 512 MB
+You are given a playlist of a radio station since its establishment. The playlist has a total of n songs.
+
+What is the longest sequence of successive songs where each song is unique?
+
+Input
+
+The first input line contains an integer n: the number of songs.
+
+The next line has n integers k1,k2,…,kn: the id number of each song.
+
+Output
+
+Print the length of the longest sequence of unique songs.
+
+Constraints
+1≤n≤2⋅105
+1≤ki≤109
+Example
+
+Input:
+8
+1 2 1 3 2 7 4 2
+
+Output:
+5
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long  ll;
@@ -29,6 +58,8 @@ main()
         
     // }
     // cout<<best;
+    
+    // *****************************************************Approach_2************************************************
     ll n,i=0,j=0,c=0;
     cin>>n;
     vector<ll> v(n);
@@ -48,14 +79,9 @@ main()
         {
             s.erase(v[i]);
             i++;
-        }
-        
-        
+        }      
     }
     cout<<c;
-    
-
-
    return 0;
 
 }
