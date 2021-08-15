5d71c10d3c61f2db6b9a5a5a581c7902159fd51b
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2021-08-15 11:36:16 -0800

    Update Coin_Combinations_I.cpp

--- Coin_Combinations_I.cpp
+++ Coin_Combinations_I.cpp
@@ -1,3 +1,38 @@
+Time limit: 1.00 s Memory limit: 512 MB
+Consider a money system consisting of n coins. Each coin has a positive integer value. Your task is to calculate the number of distinct ways you can produce a money sum x using the available coins.
+
+For example, if the coins are {2,3,5} and the desired sum is 9, there are 8 ways:
+2+2+5
+2+5+2
+5+2+2
+3+3+3
+2+2+2+3
+2+2+3+2
+2+3+2+2
+3+2+2+2
+Input
+
+The first input line has two integers n and x: the number of coins and the desired sum of money.
+
+The second line has n distinct integers c1,c2,…,cn: the value of each coin.
+
+Output
+
+Print one integer: the number of ways modulo 109+7.
+
+Constraints
+1≤n≤100
+1≤x≤106
+1≤ci≤106
+Example
+
+Input:
+3 9
+2 3 5
+
+Output:
+8
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long  ll;
@@ -20,7 +55,6 @@ main()
             if(v[j]>i) continue;
             dp[i]=(dp[i]+dp[i-v[j]])%mod;
         }
-
     }
     // for(auto c:dp)
     // cout<<c<<" ";
